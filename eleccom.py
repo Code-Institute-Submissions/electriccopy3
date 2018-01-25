4fd1f22da5232cf4fe1fbfba702e776fbe919bfb
David Evans <devans6915@aol.com>
2018-01-25 11:23:28 +0000

    Add files via upload

--- eleccom.py
+++ eleccom.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
 DBS_NAME = os.getenv('MONGO_DB_NAME', 'eleccon_clean')
-MONGODB_PORT = 27017
+#MONGODB_PORT = 27017
 COLLECTION_NAME = 'projects'
 
 
@@ -51,15 +51,15 @@ def eleccom_projects():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-#    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
-        with MongoClient(MONGO_URI) as conn:
-        # Define which collection we wish to access
-        collection = conn[DBS_NAME][COLLECTION_NAME]
-        # Retrieve a result set only with the fields defined in FIELDS
-        # and limit the the results to 55000
-        projects = collection.find(projection=FIELDS, limit=20000)
-        # Convert projects to a list in a JSON object and return the JSON data
-        return json.dumps(list(projects))
+    # with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+      with MongoClient(MONGO_URI) as conn:
+    # Define which collection we wish to access
+      collection = conn[DBS_NAME][COLLECTION_NAME]
+    # Retrieve a result set only with the fields defined in FIELDS
+    # and limit the the results to 55000
+      projects = collection.find(projection=FIELDS, limit=20000)
+    # Convert projects to a list in a JSON object and return the JSON data
+      return json.dumps(list(projects))
 
 
 if __name__ == "__main__":
